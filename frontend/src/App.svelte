ae148b6a9f971ea91bc353d43fcf228a8502e9c6
Hampton Moore <me@hamptonmoore.com>
2022-08-02 18:48:43 -0400

    Feat: basic routeset announcement

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -18,9 +18,13 @@
     let lastUpdate = "Never";
     let lastKeepalive = "Never";
 
+    let routesets = [];
+
+    let endpoint = "http://localhost:8080/"
+
     let socket;
     onMount(() => {
-        socket = new WebSocket("ws://localhost:8080/ws"); // window.location.host + "/ws"
+        socket = new WebSocket(endpoint.replace("http", "ws") + "ws"); // window.location.host + "/ws"
 
         socket.onopen = function (e) {
             console.log("ws connected");
@@ -37,13 +41,16 @@
         socket.onerror = function (e) {
             console.log("ws error", e);
         };
+        fetch(endpoint + "routesets.json").then((d)=>d.json()).then((rs)=>{
+            routesets=rs
+        })
     });
 
     let peerASN;
     let peerIP;
     let localASN;
     if (localStorage.hasOwnProperty("peerASN")) {
-        peerASN = localStorage.getItem("peerASN")
+        peerASN = Number(localStorage.getItem("peerASN"))
     } else {
         peerASN = 65530;
     }
@@ -55,7 +62,7 @@
     }
 
     if (localStorage.hasOwnProperty("localASN")) {
-        localASN = localStorage.getItem("localASN")
+        localASN = Number(localStorage.getItem("localASN"))
     } else {
         localASN = 65510;
     }
@@ -99,9 +106,52 @@
     let newAnnouncementNextHop = "192.168.100.100";
     let newAnnouncementPath = "65510, 65530, 65500";
 
+    function announceRouteset(name){
+
+        let routes = routesets[name]
+        for (let i = 0; i < routes.length; i++){
+            let route = routes[i]
+            let data = {
+                prefixes: [...route.prefixes.map((p)=>{
+                    return {prefix:p, id: generateRouteID()}
+                })],
+                origin: 0,
+                nextHop: newAnnouncementNextHop,
+                asPath: [],
+            }
+            if (route.asPath != undefined) {
+                data.asPath = route.asPath
+            }
+            if (route.nextHop != undefined){
+                data.nextHop = route.nextHop
+            }
+            socket.send(JSON.stringify({
+                type: "RouteData",
+                data: data,
+            }))
+
+            for (let j = 0; j < data.prefixes.length; j++){
+                let r = data.prefixes[j]
+                announcements.push({
+                    id: r.id,
+                    prefix: r.prefix,
+                    path: data.asPath,
+                    nexthop: data.nextHop,
+                    origin: data.origin,
+                    routeset: name
+                });
+            }
+        }
+    }
+
+    let id = 0
+    function generateRouteID(){
+        return id++;
+    }
+
     function addAnnouncement() {
         let pathArray = newAnnouncementPath.split(",").map(x => parseInt(x.trim()));
-        let routeID = new Date().getMilliseconds();  // TODO: Better ID source
+        let routeID = generateRouteID();
 
         socket.send(JSON.stringify({
             type: "RouteData",
