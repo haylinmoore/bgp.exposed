f6ca541c2fba1f4273a301db6692042562d4c957
Hampton Moore <me@hamptonmoore.com>
2022-08-01 20:05:30 -0400

    Move to gofiber and add cors

--- backend/core/core.go
+++ backend/core/core.go
@@ -5,26 +5,19 @@ import (
 	"encoding/json"
 	"flag"
 	"log"
-	"net/http"
 
-	"github.com/gorilla/websocket"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/websocket/v2"
 	"github.com/hamptonmoore/bgp.exposed/backend/bgp"
 	"github.com/hamptonmoore/bgp.exposed/backend/common"
 )
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
 
-var upgrader = websocket.Upgrader{} // use default options
-
 var server *bgp.BGPServer
 
-func ClientHandler(w http.ResponseWriter, r *http.Request) {
-	c, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Print("upgrade:", err)
-		return
-	}
-	defer c.Close()
+func ClientHandler(c *websocket.Conn) {
 	var peer *bgp.Peer
 
 	started := make(chan bool, 1)
@@ -58,7 +51,7 @@ func ClientHandler(w http.ResponseWriter, r *http.Request) {
 			log.Println(err)
 			continue
 		}
-		log.Println(packet.Type)
+
 		data, _ := json.Marshal(packet.Data)
 		if peer == nil && packet.Type == "CreateRequest" {
 			v := common.CreateRequest{}
@@ -78,10 +71,24 @@ func ClientHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	app := fiber.New()
 	server = bgp.CreateBGPServer(1000, "0.0.0.0:2000", "1.1.1.1")
-
 	flag.Parse()
 	log.SetFlags(0)
-	http.HandleFunc("/ws", ClientHandler)
-	log.Fatal(http.ListenAndServe(*addr, nil))
+
+	app.Use(cors.New())
+
+	app.Use("/ws", func(c *fiber.Ctx) error {
+		// IsWebSocketUpgrade returns true if the client
+		// requested upgrade to the WebSocket protocol.
+		if websocket.IsWebSocketUpgrade(c) {
+			c.Locals("allowed", true)
+			return c.Next()
+		}
+		return fiber.ErrUpgradeRequired
+	})
+
+	app.Get("/ws/", websocket.New(ClientHandler))
+
+	log.Fatal(app.Listen(*addr))
 }
--- go.mod
+++ go.mod
@@ -2,12 +2,22 @@ module github.com/hamptonmoore/bgp.exposed
 
 go 1.18
 
-require github.com/bgptools/fgbgp v0.0.0-20220502151704-9c60f0cb172a
+require (
+	github.com/bgptools/fgbgp v0.0.0-20220502151704-9c60f0cb172a
+	github.com/gofiber/fiber/v2 v2.35.0
+	github.com/gofiber/websocket/v2 v2.0.23
+)
 
 require (
-	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/fasthttp/websocket v1.5.0 // indirect
+	github.com/klauspost/compress v1.15.0 // indirect
+	github.com/savsgio/gotils v0.0.0-20211223103454-d0aaa54c5899 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.38.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20220329152356-43be30ef3008 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
